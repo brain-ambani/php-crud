41f0e864974b3b7b8d9fc87dcb4a8cfb1fdeef2c
Brian Ambani <brainambani1@gmail.com>
2022-11-23 02:16:01 +0300

    display skeleton created

---
+++ display.php
@@ -0,0 +1,36 @@
+<?php include 'config.php';?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Add user</button>
+    <table>
+        <thead>
+            <tr>
+                <th>Srl No.</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Mobile</th>
+                <th>Password</th>«
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Brian</td>
+                <td>brainambani1@gmail.com</td>
+                <td>074177848</td>
+                <td>1234</td>«
+            </tr>
+        </tbody>
+    </table>
+    
+</body>
+</html>
