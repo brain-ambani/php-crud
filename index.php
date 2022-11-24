d2436ec7a6c533efe7ad9ce4e68662cbfb16a97e
Brian Ambani <brainambani1@gmail.com>
2022-11-24 12:47:02 +0300

    updated form

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ if(isset($_POST['submit'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>crud operation</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 
@@ -49,7 +50,7 @@ if(isset($_POST['submit'])){
         <div>
             <input type="submit" name="submit" value="submit">
         </div>
-        
+
     </form>
     
 </body>
